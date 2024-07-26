593992e8b7fd1b7961c6f07d71b3a011facd71e1
ganin <ganin.d@selfwork.ru>
2024-07-26 19:53:43 +0300

    fix

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
 mkdir -p keyring
 curl https://ton-blockchain.github.io/global.config.json -o global.config.json
-docker run --rm -v ./keyring:/app/keyring ddd-ton-site:latest generate-adnlid > keyring/adnlid.txt
+docker run --rm -v ./keyring:/app/keyring ganindmitry63/ton-site:latest generate-adnlid > keyring/adnlid.txt
 PRIVATE_KEY="$(grep -o '^[^ ]*' keyring/adnlid.txt)"
 ADNL="$(grep -o '[[:space:]].*' keyring/adnlid.txt | sed 's/ //g')"
 echo "PRIVATE_KEY: $PRIVATE_KEY - LINK AS TON Site on https://dns.ton.org/"
